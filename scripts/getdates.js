a2898e3f99611cac694475a33e0a4f162cf17fd9
chemcarl08 <chemcarl08@hotmail.com>
2024-06-17 00:17:48 -0600

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,15 +1,12 @@
-// Function to update the current year in the footer
 function updateCopyrightYear() {
     const currentYear = new Date().getFullYear();
     document.getElementById('currentyear').textContent = currentYear;
 }
 
-// Function to update the last modified date in the footer
 function updateLastModified() {
     const lastModifiedDate = document.lastModified;
     document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
 }
 
-// Call functions to update the footer content
 updateCopyrightYear();
 updateLastModified();
